7d9a134e9a073a2345d261ccfe4874bd198deb36
Yuan Chen <godec52217@gmail.com>
2024-09-05 13:03:34 +0200

    Add type information

--- src/models/InputDataTypes.ts
+++ src/models/InputDataTypes.ts
@@ -1,50 +1,59 @@
 import { Instant } from '@js-joda/core';
 
-// src/types/Address.ts
-export interface Address {
-  address1?: string | null; // Optional: First line of the address
-  address2?: string | null; // Optional: Second line of the address
-  street?: string | null; // Optional: Street of the address
-  city?: string | null; // Optional: City of the address
-  postalCode?: string | null; // Optional: Postal code of the address
-  country?: string | null; // Optional: Country of the address
+// https://github.com/cph-cachet/carp-webservices-spring/blob/master/docs/input-data-types.md
+export interface InputDataType {
+  __type: string;
 }
 
-export interface Diagnosis {
+export interface Address extends InputDataType {
+  __type: 'dk.carp.webservices.input.address';
+  address1?: string | null;
+  address2?: string | null;
+  street?: string | null;
+  city?: string | null;
+  postalCode?: string | null;
+  country?: string | null;
+}
+
+export interface Diagnosis extends InputDataType {
+  __type: 'dk.carp.webservices.input.diagnosis';
   effectiveDate?: Instant | null;
   diagnosi?: string | null;
   icd11Code: string;
   conclusion?: string | null;
 }
 
-export interface FullName {
+export interface FullName extends InputDataType {
+  __type: 'dk.carp.webservices.input.full_name';
   firstName?: string | null;
   middleName?: string | null;
   lastName?: string | null;
 }
 
-export interface InformedConsent {
-  signedTimestamp?: Instant | null; // Optional: Time consent was signed
-  signedLocation?: string | null; // Optional: Location consent was signed``
-  userId?: string | null; // Optional: User ID of the participant
-  name: string; // Required: Name of the participant
-  consent?: string | null; // Optional: Content of the signed consent
-  signatureImage?: string | null; // Optional: Signature image in PNG format
+export interface InformedConsent extends InputDataType {
+  __type: 'dk.carp.webservices.input.informed_consent';
+  signedTimestamp?: Instant | null;
+  signedLocation?: string | null;
+  userId?: string | null;
+  name: string;
+  consent?: string | null;
+  signatureImage?: string | null;
 }
 
-export interface PhoneNumber {
-  countryCode: string; // Country code as a string
-  icoCode?: string | null; // Optional: ICO 3166 code
-  number: string; // Phone number as a string
+export interface PhoneNumber extends InputDataType {
+  __type: 'dk.carp.webservices.input.phone_number';
+  countryCode: string;
+  icoCode?: string | null;
+  number: string;
 }
 
-export interface SocialSecurityNumber {
-  socialSecurityNumber: string; // Social security number (SSN)
-  country: string; // Country of issuance
+export interface SocialSecurityNumber extends InputDataType {
+  __type: 'dk.carp.webservices.input.ssn';
+  socialSecurityNumber: string;
+  country: string;
 }
 
-export enum Sex {
-  Male = 'Male',
-  Female = 'Female',
-  Intersex = 'Intersex',
+export interface Sex extends InputDataType {
+  __type: 'dk.cachet.carp.input.sex';
+  value: 'Male' | 'Female' | 'Intersex';
 }
