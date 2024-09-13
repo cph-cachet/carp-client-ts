724c2ed9eb0adbf09cea6b13baf8aa3082830133
Daniel Jakab <jakdan99@gmail.com>
2024-09-13 10:33:41 +0200

    Implement InactiveDeployments

--- src/models/ParticipantGroups.ts
+++ src/models/ParticipantGroups.ts
@@ -1,3 +1,7 @@
+import carpCommon from '@cachet/carp-common';
+import cdk = carpCommon.dk;
+import UUID = cdk.cachet.carp.common.application.UUID;
+
 export interface ParticipantGroups {
   groups: ParticipantGroup[];
   groupStatuses: any[];
@@ -50,4 +54,13 @@ export interface ParticipantStatus {
     roleNames: string[];
   };
   assignedPrimaryDeviceRoleNames: string[];
+}
+
+export interface InactiveDeployment {
+  deploymentId: UUID;
+  dateOfLastDataUpload: {
+    epocSeconds: number;
+    'value$kotlinx_datetime': Date;
+    nanosecondsOfSecond: number;
+  };
 }
